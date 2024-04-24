93c5a0b0bc6eb2437cb502ef65ddf3af602196d2
MyaPwintEliza <104987808+MyaPwintEliza@users.noreply.github.com>
2024-04-25 01:00:37 +0630

    first commit

---
+++ src/components/titles.tsx
@@ -0,0 +1,9 @@
+import Logo from "../assets/images/logo.svg";
+
+export const Title = () => {
+  return (
+    <div className="mx-auto">
+      <img src={Logo} alt="" />
+    </div>
+  );
+};
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { cn } from "../../lib/utils";
+
+type ButtonProps = React.ComponentPropsWithoutRef<"button">;
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ disabled, className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(
+          "rounded-lg px-4 py-4 text-2xl font-bold text-white transition hover:bg-neutral-light hover:text-primary-dark focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-25 sm:px-10",
+          {
+            "bg-primary-dark": !disabled,
+          },
+          {
+            "bg-neutral": disabled,
+          },
+          className
+        )}
+        disabled={disabled}
+        {...props}
+      />
+    );
+  }
+);
