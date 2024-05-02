6d77a75f419c4a196b7e3e1c425b358b0e46fdf2
MyaPwintEliza <104987808+MyaPwintEliza@users.noreply.github.com>
2024-05-02 17:45:27 +0630

    Update titles.tsx

--- src/components/titles.tsx
+++ src/components/titles.tsx
@@ -1,9 +1,9 @@
-const photo = require("./Logo.svg");
+// const photo = require("./Logo.svg");
 
-export const Title = () => {
-  return (
-    <div className="mx-auto">
-      <img src={photo} alt="" />
-    </div>
-  );
-};
+// export const Title = () => {
+//   return (
+//     <div className="mx-auto">
+//       <img src={photo} alt="" />
+//     </div>
+//   );
+// };
